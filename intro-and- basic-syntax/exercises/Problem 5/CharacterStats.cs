1bac94884284e3f38cf3cf5323738e2ceab231a2
Darknia <radina.kolcheva@gmail.com>
2018-03-17 18:39:23 +0200

    Issues in problem 5 fixed

--- intro-and- basic-syntax/exercises/Problem 5/CharacterStats.cs
+++ intro-and- basic-syntax/exercises/Problem 5/CharacterStats.cs
@@ -13,13 +13,13 @@ namespace CharacterStats
             //We receive character name
             String name = Console.ReadLine();
             //Character health
-            int currHealth = int.Parse(Console.ReadLine());
+            int currentHealth = int.Parse(Console.ReadLine());
             //Maximum Character Health
-            int maxHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
             //Current Character Energy
-            int currEnergy = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
             //Maximum Character Energy
-            int maxEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
             
             //Printing information about the character
             Console.WriteLine("Name: {0}\nHealth: |{1}|\nEnergy: |{2}|", 
