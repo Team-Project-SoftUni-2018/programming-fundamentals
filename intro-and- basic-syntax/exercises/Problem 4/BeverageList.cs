a62324b5e82b411b41a885e24841b422d22d66bb
delyandamyanov <delyan.damyanov@outlook.com>
2018-03-17 16:36:24 +0200

    Added Solution to Problem 4

---
+++ intro-and- basic-syntax/exercises/Problem 4/BeverageList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = Console.ReadLine();
+            int vol = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}ml {1}:\n{2}kcal, {3}g sugars", vol, name, ((energy * vol)/100.0), ((sugar * vol)/100.0));
+        }
+    }
+}
---
+++ intro-and- basic-syntax/exercises/Problem 5/CharacterStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = Console.ReadLine();
+            int currHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Name: {0}\nHealth: |{1}|\nEnergy: |{2}|", 
+                name, new String('|', currHealth) + new string('.', maxHealth - currHealth), 
+                new String('|', currEnergy) + new string('.', maxEnergy - currEnergy));
+        }
+
+    }
+    
+}
