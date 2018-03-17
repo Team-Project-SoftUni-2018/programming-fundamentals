c82325720182c5dbe76d3dcede89a52f0690446a
evelinturiyski <elektross@abv.bg>
2018-03-17 16:10:59 +0200

    Added problem 3

---
+++ intro-and- basic-syntax/exercises/Problem 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = 1.60934;
+            Console.WriteLine($"{a * b:f2}");
+
+        }
+    }
+}
