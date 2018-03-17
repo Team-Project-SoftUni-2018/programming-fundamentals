67d19d42e8eec571cdabdb8ce33a50456f6cf9a3
MitkoPetkov <mitko_petkoff@abv.bg>
2018-03-17 15:57:41 +0200

    added problem one

---
+++ intro-and- basic-syntax/exercises/Problem 1/Problem 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.Write($"{num1:d4} ");
+            Console.Write($"{num2:d4} ");
+            Console.Write($"{num3:d4} ");
+            Console.Write($"{num4:d4} ");
+        }
+    }
+}
