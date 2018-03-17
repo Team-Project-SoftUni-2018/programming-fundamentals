7e33e5d83de702c8fe89dea32678cd17632ec162
delyandamyanov <delyan.damyanov@outlook.com>
2018-03-17 17:35:05 +0200

    Fixed Issue for Problem 4

--- intro-and- basic-syntax/exercises/Problem 4/BeverageList.cs
+++ intro-and- basic-syntax/exercises/Problem 4/BeverageList.cs
@@ -11,7 +11,7 @@ namespace Beverages
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
-            int volume = int.Parse(Console.ReadLine());
+            int vol = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int sugar = int.Parse(Console.ReadLine());
 
